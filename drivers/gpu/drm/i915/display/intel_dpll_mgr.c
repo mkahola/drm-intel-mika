b09cf3d843e4ab8b4bb0f6293d35a42109878be8
Mika Kahola <mika.kahola@intel.com>
2025-06-19 15:11:35 +0300

    drm/i915/display: Add .get_hw_state to MTL+ platforms
    Add .get_hw_state hook to MTL+ platforms for dpll framework.
    
    Signed-off-by: Mika Kahola <mika.kahola@intel.com>

--- drivers/gpu/drm/i915/display/intel_dpll_mgr.c
+++ drivers/gpu/drm/i915/display/intel_dpll_mgr.c
@@ -4360,9 +4360,27 @@ static void mtl_pll_disable(struct intel_display *display,
 	intel_mtl_pll_disable(encoder, pll);
 }
 
+static bool mtl_pll_get_hw_state(struct intel_display *display,
+				 struct intel_dpll *pll,
+				 struct intel_dpll_hw_state *dpll_hw_state)
+{
+	struct intel_encoder *encoder = get_intel_encoder(display, pll);
+
+	if (!encoder)
+	{
+		drm_dbg_kms(display->drm, "encoder not found\n");
+		return false;
+	}
+
+	intel_cx0pll_readout_hw_state(encoder, &dpll_hw_state->cx0pll);
+
+	return true;
+}
+
 static const struct intel_dpll_funcs mtl_pll_funcs = {
 	.enable = mtl_pll_enable,
 	.disable = mtl_pll_disable,
+	.get_hw_state = mtl_pll_get_hw_state,
 };
 
 static const struct dpll_info mtl_plls[] = {
